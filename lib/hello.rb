71592d64456b3ee4b906117409e9be850e5d29b5
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-23 21:49:36 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,17 @@
-names = ["Tim", "Tom", "Jim"]
-
-def hello_t(arr)
-i = 0 
-while i < arr.length 
-yield arr[i]
-i = i + 1 
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
-end
-
 # call your method here!
 
 
